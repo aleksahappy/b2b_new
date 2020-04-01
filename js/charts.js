df5df413641614ac5b8401bb7d94da2f1ed9c26f
PavPavv <pavleaks@gmail.com>
2020-04-01 11:16:21 +0400

    Добавил новые графики для рабочего стола с помощью библиотеки charts.js

--- js/charts.js
+++ js/charts.js
@@ -1,55 +1,88 @@
 'use strict';
+let inValue = [120000000, 30000000];
+let inAmount = [20000, 5000];
 
-//'Payments' chart
-google.charts.load("current", { packages: ["corechart"] });
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-  let data = google.visualization.arrayToDataTable([
-    ['Task', ''],
-    ['Товар в наличии', 120000],
-    ['ДЗ', 30000]
-  ]);
+//payment chart
+const paymentChart = () => {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'doughnut',
 
-  let options = {
-    title: '',
-    pieHole: 0.8,
-    colors: ['#DFA12A', '#E7575F'],
-    legend: {
-      position: 'none'
-    },
-    chartArea: {
-      width: '100%'
-    },
-    pieSliceText: 'value',
-    pieSliceTextStyle: {
-      color: '#34495E',
-      fontSize: 20
-    }
+      // The data for our dataset
+      data: {
+          labels: ['', ''],
+          datasets: [{
+              label: false,
+              backgroundColor: ['#DFA12A', '#E7575F'],
+              borderColor: 'transparent',
+              data: [inValue[0], inValue[1]]
+          }]
+      },
 
-  };
+      // Configuration options go here
+      options: {
+        cutoutPercentage: 80,
+        legend: {
+          display: false
+        }
+      }
+  });
 
-  let chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-  chart.draw(data, options);
-}
+  //display payment chart info
+  const donutSum = document.querySelector('.donut-sum');
+  const freeProduct = document.querySelector('.free-product');
+  const deptSum = document.querySelector('.dept-sum');
 
-//'Products' chart
-// google.charts.load("current", { packages: ["corechart"] });
-// google.charts.setOnLoadCallback(drawChart1);
-// function drawChart1() {
-//   let data = google.visualization.arrayToDataTable([
-//     ['Task', 'Hours per Day'],
-//     ['Work', 11],
-//     ['Eat', 2],
-//     ['Commute', 2],
-//     ['Watch TV', 2],
-//     ['Sleep', 7]
-//   ]);
+  freeProduct.innerHTML = inValue[0] + ' р.';
+  deptSum.innerHTML = inValue[1] + ' р.';
+  let sum = inValue[0] + inValue[1];
+  donutSum.innerHTML = sum + ' р.';
 
-//   let options = {
-//     title: 'My Daily Activities',
-//     pieHole: 0.4,
-//   };
+};
 
-//   let chart = new google.visualization.PieChart(document.getElementById('donutchart1'));
-//   chart.draw(data, options);
-// }
+//products chart
+const productsChart = () => {
+  const ctx = document.getElementById('myChart1').getContext('2d');
+  const chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'doughnut',
+
+      // The data for our dataset
+      data: {
+          labels: ['', ''],
+          datasets: [{
+              label: false,
+              backgroundColor: ['#2A659D', '#DFA12A'],
+              borderColor: 'transparent',
+              data: [inValue[0], inValue[1]]
+          }]
+      },
+
+      // Configuration options go here
+      options: {
+        cutoutPercentage: 80,
+        legend: {
+          display: false
+        }
+      }
+  });
+
+  //display payment chart info
+  const donutSum1 = document.querySelector('#donut-sum1');
+  const stockProduct = document.querySelector('.stock-product');
+  const stockSum = document.querySelector('.stock-sum');
+
+  stockProduct.innerHTML = inAmount[0] + ' шт.';
+  stockSum.innerHTML = inAmount[1] + ' шт.';
+  let sum = inAmount[0] + inAmount[1];
+  donutSum1.innerHTML = sum + ' шт.';
+
+};
+
+
+
+
+
+paymentChart();
+productsChart();
