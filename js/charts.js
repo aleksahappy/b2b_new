dea51369a5dc072be57be53cc0f9cf3cccf80c37
PavPavv <pavleaks@gmail.com>
2020-05-19 16:39:52 +0400

    Добавил блок "Заказы в работе" с графиком и таблицей

--- js/charts.js
+++ js/charts.js
@@ -1,31 +1,73 @@
 'use strict';
-let inValue = [120000000, 30000000];
-let inAmount = [20000, 5000];
+//  тестовые данные
+// кол-во заказов
+let data1 = [4, 3, 2, 1];
+//  суммы состояний заказов
+let data1Sum = [203600, 50000, 530000, 1000000];
 
-//payment chart
-const paymentChart = () => {
+let test1 = arraySum(data1);
+let test2 = arraySum(data1Sum);
+
+//  Находит сумму элементов массива
+function arraySum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+
+// График "Заказы в работе"
+const ordersProgress = () => {
+  const ordersInfo = document.querySelector('.orders-info');
+  ordersInfo.textContent = `${test1} активных заказов на общую сумму ${test2} руб.`;
   const ctx = document.getElementById('myChart').getContext('2d');
+  ctx.canvas.parentNode.style.height = '393px';
+  ctx.canvas.parentNode.style.width = '393px';
+
   const chart = new Chart(ctx, {
-      // The type of chart we want to create
+      // тип графика
       type: 'doughnut',
 
-      // The data for our dataset
+      // Отображение данных
       data: {
-          labels: ['', ''],
+          //  В тултипах при наведение на шкалы графика
+          labels: [`Ожидается заказов`, `Товар отгружен: заказов`, `Собрано заказов`, `В наличии`],
+          //  Настройка отображения данных
           datasets: [{
+              //
               label: false,
-              backgroundColor: ['#DFA12A', '#E7575F'],
+              //  цвета шкал графика
+              backgroundColor: ['#96B6D3', '#9FCB93', '#B5A6BB', '#FBCD80'],
+              //  цвет бордера шкал и графика
               borderColor: 'transparent',
-              data: [inValue[0], inValue[1]]
+              //  данные для отображения
+              data: [data1[0], data1[1], data1[2], data1[3]]
           }]
       },
 
-      // Configuration options go here
+      // Настройки отображения графика
       options: {
-        cutoutPercentage: 80,
+        // ширина "кольца"
+        cutoutPercentage: 70,
+        // отключение легенды
         legend: {
           display: false
-        }
+        },
+        //  поворот угла стартового значения
+        rotation: 5,
+        //  отступы графика
+        layout: {
+          padding: {
+            left: 10,
+            right: 10,
+            top: 10,
+            bottom: 10
+          }
+        },
+        //  отклюдчаем лишнее свободное пространство вокруг графика
+        maintainAspectRatio: false
       }
   });
 
@@ -34,55 +76,10 @@ const paymentChart = () => {
   const freeProduct = document.querySelector('.free-product');
   const deptSum = document.querySelector('.dept-sum');
 
-  freeProduct.innerHTML = inValue[0] + ' р.';
-  deptSum.innerHTML = inValue[1] + ' р.';
-  let sum = inValue[0] + inValue[1];
-  donutSum.innerHTML = sum + ' р.';
+  // freeProduct.innerHTML = inValue[0] + ' р.';
+  // deptSum.innerHTML = inValue[1] + ' р.';
+  // let sum = inValue[0] + inValue[1];
+  // donutSum.innerHTML = sum + ' р.';
 
 };
-
-//products chart
-const productsChart = () => {
-  const ctx = document.getElementById('myChart1').getContext('2d');
-  const chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: 'doughnut',
-
-      // The data for our dataset
-      data: {
-          labels: ['', ''],
-          datasets: [{
-              label: false,
-              backgroundColor: ['#2A659D', '#DFA12A'],
-              borderColor: 'transparent',
-              data: [inValue[0], inValue[1]]
-          }]
-      },
-
-      // Configuration options go here
-      options: {
-        cutoutPercentage: 80,
-        legend: {
-          display: false
-        }
-      }
-  });
-
-  //display payment chart info
-  const donutSum1 = document.querySelector('#donut-sum1');
-  const stockProduct = document.querySelector('.stock-product');
-  const stockSum = document.querySelector('.stock-sum');
-
-  stockProduct.innerHTML = inAmount[0] + ' шт.';
-  stockSum.innerHTML = inAmount[1] + ' шт.';
-  let sum = inAmount[0] + inAmount[1];
-  donutSum1.innerHTML = sum + ' шт.';
-
-};
-
-
-
-
-
-paymentChart();
-productsChart();
+ordersProgress();
