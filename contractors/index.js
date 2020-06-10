e765a881157aa1dabadbe46e229efbc05807861c
PavPavv <pavleaks@gmail.com>
2020-06-10 18:08:07 +0400

    Начал делать модальное окно в Контрагентах

--- contractors/index.js
+++ contractors/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var addContraBtn = document.querySelector('#addContraBtn');
+var contraModal = document.querySelector('#contraModal');
+var closeContraModal = document.querySelector('#addContra .close-btn');
+
+addContraBtn.addEventListener('click', () => {
+  contraModal.style.display = 'block';
+});
+
+closeContraModal.addEventListener('click', () => {
+  contraModal.style.display = 'none';
+});
