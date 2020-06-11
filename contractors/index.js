3fbb472d74b399ca5dae588fcba2c6141f141566
PavPavv <pavleaks@gmail.com>
2020-06-11 13:06:28 +0400

    доделал модальное окно "Добавить контрагента"

--- contractors/index.js
+++ contractors/index.js
@@ -1,13 +1,34 @@
 'use strict';
 
 var addContraBtn = document.querySelector('#addContraBtn');
+var addContraBtnMob = document.querySelector('#addContraBtnMob');
 var contraModal = document.querySelector('#contraModal');
 var closeContraModal = document.querySelector('#addContra .close-btn');
 
-addContraBtn.addEventListener('click', () => {
-  contraModal.style.display = 'block';
-});
+function contraModalWin() {
+  addContraBtn.addEventListener('click', () => {
+    contraModal.style.display = 'block';
+  });
 
-closeContraModal.addEventListener('click', () => {
-  contraModal.style.display = 'none';
-});
+  addContraBtnMob.addEventListener('click', () => {
+    contraModal.style.display = 'block';
+  });
+
+  closeContraModal.addEventListener('click', () => {
+    contraModal.style.display = 'none';
+  });
+
+  contraModal.addEventListener('click', function (event) {
+      if (event.target === this) {
+        contraModal.style.display = 'none';
+      }
+  });
+
+  window.addEventListener('keydown', function (event) {
+      if(event.key === 'Escape') {
+        contraModal.style.display = 'none';
+      }
+  });
+
+}
+contraModalWin();
