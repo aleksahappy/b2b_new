ea07925501d0e2226f7efaf3a8ec8e6ae3afda07
PavPavv <pavleaks@gmail.com>
2020-07-21 13:23:44 +0400

    Доделал прогресс бар в Рекламации

--- reclamation/index.js
+++ reclamation/index.js
@@ -3,7 +3,7 @@ var headPan = document.querySelector('.row.head-pan');
 var closeChat = document.querySelector('.close-chat');
 var chatWrap = document.querySelector('.chat-wrap');
 var progressBar = document.querySelector('.progress-bar');
-var progressTitle = document.querySelectorAll('.progress-title');
+var progressItems = document.querySelectorAll('.progress-item');
 var reclState = {};
 
 
@@ -37,7 +37,7 @@ closeChat.addEventListener('click', function() {
 
 function getReclStatus(data) {
   for (let key in data) {
-    if (key === 'status1' || key === 'status2' || key === 'status3') {
+    if (key === 'status1' || key === 'status2' || key === 'status3' || key === 'status4') {
       reclState[key] = data[key];
     }
   }
@@ -45,7 +45,9 @@ function getReclStatus(data) {
 
 
 function putReclStatus() {
+  var refundBtn = document.querySelector('#refund-btn');
   var progressBar = document.querySelector('.progress-bar');
+  var process = document.querySelector('.point.wait');
   var result = document.querySelector('.point.result');
   var resultTitle = result.closest('progress-title');
   var done = document.querySelector('.point.undone');
@@ -57,25 +59,52 @@ function putReclStatus() {
 
       if (prop === 'status1') {
         console.log('Do something with status1');
+        refundBtn.className = 'sub-act btn disabled';
       }
+
       if (prop === 'status2') {
         console.log('Do something with status2');
+        refundBtn.className = 'btn act';
+        process.className = 'point process';
+        for (let i = 0; i < progressItems.length; i++) {
+          var processTitle = progressItems[1].querySelector('.progress-title');
+
+          processTitle.classList.remove('non-active');
+          processTitle.classList.add('active');
+        }
+      }
+
+      if (prop === 'status3') {
+        console.log('Do something with status3');
+        refundBtn.className = 'btn act';
+        process.className = 'point process';
         result.className = 'point denied';
         progressBar.removeChild(parentDone);
         for (let i = 0; i < progressItems.length; i++) {
+          var processTitle = progressItems[1].querySelector('.progress-title');
           var resultTitle = progressItems[2].querySelector('.progress-title');
+
+          processTitle.classList.remove('non-active');
+          processTitle.classList.add('active');
           resultTitle.innerHTML = 'Не удоволетворена';
           resultTitle.classList.remove('non-active');
           resultTitle.classList.add('denied');
         }
       }
-      if (prop === 'status3') {
-        console.log('Do something with status3');
+
+      if (prop === 'status4') {
+        console.log('Do something with status4');
+        refundBtn.className = 'btn act';
+        process.className = 'point process';
         result.className = 'point accepted';
         done.className = 'point done';
         for (let i = 0; i < progressItems.length; i++) {
+          var processTitle = progressItems[1].querySelector('.progress-title');
           var statusTitle = progressItems[2].querySelector('.progress-title');
           var resultTitle = progressItems[3].querySelector('.progress-title');
+
+          processTitle.classList.remove('non-active');
+          processTitle.classList.add('active');
           statusTitle.innerHTML = 'Удоволетворена';
           statusTitle.classList.add('accepted');
           resultTitle.classList.remove('non-active');
@@ -91,20 +120,3 @@ function setChatPos() {
   chatWrap.style.top = headPanCoords.top + headPan.offsetHeight + 'px';
 }
 setChatPos();
-
-
-// function setTitleRight() {
-//   var result = 0;
-//   for (let i = 0; i < progressTitle.length; i++) {
-//     var closestIc = progressTitle[i].nextElementSibling.nextElementSibling;
-//     result = (progressTitle[i].offsetWidth + closestIc.offsetWidth);
-//   }
-//   console.log(result);
-// }
-// setTitleRight();
-//
-//
-// function getRightPos(title, icon) {
-//   var right = (title.offsetWidth - icon.offsetWidth) / 2;
-//   return '-' + right + 'px';
-// }
