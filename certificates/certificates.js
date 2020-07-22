d16b185b829649a4fbcd9b3e4cdee954c5d85821
Aleksahappy <aleksa.happy@gmail.com>
2020-07-22 09:32:10 +0400

    Документы и сертификаты: правка ошибок

--- certificates/certificates.js
+++ certificates/certificates.js
@@ -36,7 +36,7 @@ function startCertPage() {
   })
   .catch(err => {
     console.log(err);
-    initPage();
+    loader.hide();
   });
 }
 
--- documents/documents.js
+++ documents/documents.js
@@ -37,6 +37,6 @@ function initPage(data) {
                 </div>`
     }]
   }
-  initTable('#docs-table', settings);
+  initTable('#docs', settings);
   loader.hide();
 }
