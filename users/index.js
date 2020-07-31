8b0e354ce042f742ebf491f44b9fbacbd2b3c5dc
Aleksahappy <aleksa.happy@gmail.com>
2020-07-31 23:23:55 +0400

    Страница "Пользователи":
    Правка верстки:
    - исправлена верстка заголовка страницы в соответствии с артбордом
    - оставлена только одна форма добавления/регистрации пользователя
    - оставлена только одна адаптивная таблица
    - исправлена верстка формы добавления/регистрации пользователя
    - переделана верстка адаптивной таблицы
    
    Правка стилей:
    - полностью исправлены стили и адаптивные стили страницы
    - стили блоков информации по максимому теперь берутся из main.css
    
    Правка json:
    - изменен тестовый json страницы (переименованы и добавлены поля)
    
    Правка функционала:
    - переписана инициализация страницы
    - добавлено условие отображения данных страницы для пользователя с полным доступом и без (accessType)
    - добавлена функция для изменения входящих данных (convertData)
    - добавлена функция для открытия формы добавления/регистрации пользователя, которая меняет название формы чистит или заполняет форму данными в зависимости от действия (добавление или правка)

--- users/index.js
+++ users/index.js
@@ -1,159 +1,138 @@
 "use strict";
-var usersTable = document.querySelector("#users-table");
-var usersModal = document.querySelector("#new-user-modal");
-var editUserModal = document.querySelector("#edit-user-modal");
-var closeEditUserModal = document.querySelector("#edit-user-modal .close");
 
+// Константы:
 
-// Запуск данных таблицы пользователей:
+// здесь мы как-то определяем какой доступ у пользователя, который просматривает данную страницу
+// если у пользователя полный доступ, то отображаем колонку с тоглами, если нет - не выводим
+// (сейчас для демонстрации доступ полный)
+var accessType = 'full';
 
-function startUsersTable() {
+// Динамическе переменные:
+
+var items = [];
+
+// Запускаем рендеринг страницы пользователей:
+
+startUsersPage();
+
+// Запуск страницы пользователей:
+
+function startUsersPage() {
   sendRequest(`../json/users_data.json`)
-    //sendRequest(urlRequest.main, {action: 'dashboardTable'})
-    .then((result) => {
-      var data = JSON.parse(result);
-      loader.hide();
-      data = convertData(data);
-      var settings = {
-        data: data,
-        head: true,
-        cols: [
-          {
-            title: 'Доступ',
-            content: '  <div class="toggle" onclick="toggle(event)"><div class="toggle-in"></div></div>'
-          },
-          {
-            key: 'contr_fio',
-            title: 'ФИО',
-            content: ''
-          },
-          {
-            key: 'contr_sex',
-            title: 'Пол',
-            content: ''
-          },
-          {
-            key: 'contr_birth',
-            title: 'Дата рождения',
-            content: ''
-          },
-          {
-            key: 'contr_tel',
-            title: 'Телефон',
-            content: ''
-          },
-          {
-            key: 'contr_mail',
-            title: 'Email',
-            content: ''
-          },
-          {
-            key: 'contr_access',
-            title: 'Тип доступа',
-            content: '<div class="row"><div class="pill">#contr_access#</div></div>'
-          },
-          {
-            key: 'contr_date',
-            title: 'Дата заведения',
-            content: ''
-          },
-          {
-            key: 'contr_status',
-            title: 'Должность'
-          },
-          {
-            key: '',
-            title: 'Редактировать',
-            content: '<div class="edit icon"  onclick="openPopUp("#edit-user-modal")"></div>'
-          }
-        ]
-      };
-      initTable("#users-table", settings);
-      var usersTabletData = {
-        area: "#users-table-tablet",
-        items: data,
-        action: "replace",
-      };
-      var usersMobData = {
-        area: "#users-table-mob",
-        items: data,
-        action: "replace",
-      };
-      fillTemplate(usersTabletData);
-      fillTemplate(usersMobData);
-      accessTableType();
-      initForm('#new-user-modal', testNewUser);
-      initForm('#edit-user-modal', testNewUser);
-      initCalendar('#user-birth');
-      initCalendar('#edit-user-birth');
-    })
-    .catch((err) => {
-      console.log(err);
-      loader.hide();
-      // initTable("#users-table");
-    });
+  //sendRequest(urlRequest.main, {action: 'users'})
+  .then(result => {
+    items = JSON.parse(result);
+    convertData();
+    initPage();
+  })
+  .catch(err => {
+    console.log(err);
+    initPage();
+  });
 }
-startUsersTable();
 
+// Инициализация страницы:
 
-//  Определение расцветки стикера статуса доступа в зависсимости от поданных
-
-function accessTableType() {
-  var tbody = usersTable.querySelector("tbody");
-  var trs = tbody.querySelectorAll("tr");
-  var access = tbody.querySelectorAll(".pill");
-  var usersTableTablet = document.querySelector("#users-table-tablet");
-  var usersTableMob = document.querySelector("#users-table-mob");
-
-  for (let i = 0; i < access.length; i++) {
-    if (access[i].innerHTML === "частичный") {
-      access[i].classList.add("access");
-      access[i].classList.add("limit");
-      var trr = access[i].closest('tr');
-      var tggll = trr.querySelector('.toggle');
-      tggll.classList.add('checked');
-
-    } else if (access[i].innerHTML === "полный") {
-      access[i].classList.add("access");
-      access[i].classList.add("full");
-      var trr = access[i].closest('tr');
-      var tggll = trr.querySelector('.toggle');
-      tggll.classList.add('checked');
-
-    } else if (access[i].innerHTML === "отключен") {
-      access[i].classList.add("access");
-      access[i].classList.add("off");
-    }
+function initPage() {
+  if (accessType !== 'full') {
+    changeCss('#users-table th:nth-child(1)', 'display', 'none');
+    changeCss('#users-table td:nth-child(1)', 'display', 'none');
   }
+  var settings = {
+    data: items,
+    head: true,
+    cols: [{
+      title: 'Доступ',
+      content: '<div class="toggle #toggle#" onclick="toggle(event, #id#)"><div class="toggle-in"></div></div>'
+    }, {
+      key: 'fio',
+      title: 'ФИО',
+      sort: 'text',
+      filter: 'search'
+    }, {
+      key: 'gender',
+      title: 'Пол',
+      sort: 'text',
+      filter: 'full'
+    }, {
+      key: 'birth',
+      title: 'Дата рождения',
+      sort: 'date',
+      filter: 'search'
+    }, {
+      key: 'tel',
+      title: 'Телефон',
+      content: '<a href="tel:#tel#">#tel#</a>'
+    }, {
+      key: 'email',
+      title: 'Email',
+      content: '<a href="mailto:#email#">#email#</a>'
+    }, {
+      key: 'access',
+      title: 'Тип доступа',
+      sort: 'text',
+      filter: 'full',
+      content: '<div class="pill access #status#">#access#</div>'
+    }, {
+      key: 'date',
+      title: 'Дата заведения',
+      sort: 'date',
+      filter: 'search'
+    }, {
+      key: 'position',
+      title: 'Должность',
+      sort: 'text',
+      filter: 'search'
+    }, {
+      key: '',
+      title: 'Редактировать',
+      content: `<div class="edit icon" onclick="openUserPopUp('#id#')"></div>`
+    }]
+  };
+  initTable("#users-table", settings);
+  fillTemplate({
+    area: "#users-table-adaptive",
+    items: items
+  });
+  initForm('#user-form');
+  loader.hide();
+}
 
+// Преобразование полученных данных:
 
-  //  Вспомогательная функция для подсветки иконок статусов в таблицах в соответствии с данными
-
-  function checkStatusIcon(table) {
-    var infoblocks = table.querySelectorAll(".infoblock");
-
-    for (let i = 0; i < infoblocks.length; i++) {
-      var statusIc = infoblocks[i].querySelector(".user-status .icon");
-      var checkAccessVal = infoblocks[i].querySelector(".check-access-value");
-      var headToggle = infoblocks[i].querySelector(".toggle.dark");
-
-      if (!headToggle.classList.contains("on")) {
-        if (checkAccessVal.innerHTML == "частичный") {
-          statusIc.classList.add("limited");
-          headToggle.classList.add("checked");
-
-        } else if (checkAccessVal.innerHTML == "полный") {
-          statusIc.classList.add("boundless");
-          headToggle.classList.add("checked");
-
-        } else if (checkAccessVal.innerHTML == "отключен") {
-          statusIc.classList.add("denied");
-        }
-      } else {
-        statusIc.classList.add("denied");
-      }
-    }
+function convertData() {
+  if (!items) {
+    items = [];
   }
-  checkStatusIcon(usersTableTablet);
-  checkStatusIcon(usersTableMob);
+  items.forEach(el => {
+    var status = '',
+        toggle = 'checked';
+    if (el.access === 'полный') {
+      status = 'full';
+    } else if (el.access === 'частичный') {
+      status = 'limit';
+    } else if (el.access === 'отключен') {
+      status = 'off';
+      toggle = '';
+    }
+    el.status = status;
+    el.toggle = toggle;
+    el.accessType = accessType === 'full' ? '' : 'displayNone';
+  });
+}
+
+// Открытие всплывающего окна с формой:
+
+function openUserPopUp(id) {
+  var userPopUp = getEl('#user'),
+      title = getEl('.pop-up-title .title', userPopUp);
+  if (id) {
+    title.textContent = 'Редактировать пользователя';
+    var data = items.find(el => el.id == id);
+    fillForm('#user-form', data);
+  } else {
+    title.textContent = 'Новый пользователь';
+    // clearForm('#user'); - здесь очистить форму при необходимости
+  }
+  openPopUp(userPopUp);
 }
