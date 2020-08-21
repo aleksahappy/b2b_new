31f2abb20e6b79848da9669917566366fda0bf22
PavPavv <pavleaks@gmail.com>
2020-08-21 16:25:28 +0400

    - Поправил мобтльное меню

--- reclamation/index.js
+++ reclamation/index.js
@@ -129,3 +129,25 @@ function openChat(el) {
     icon.style.visibility = 'hidden';
   }
 }
+
+
+// Преобразование полученных данных:
+function convertData(data) {
+  if (!data) {
+    return [];
+  }
+  data.forEach(el => {
+    el.order_sum = convertPrice(el.order_sum);
+    var sum;
+    for (var i = 1; i <= 5; i++) {
+      sum = el[`sum${i}`];
+      if (sum && sum != 0) {
+        el[`sum${i}`] = convertPrice(sum);
+        el[`display${i}`] = '';
+      } else {
+        el[`display${i}`] = 'displayNone';
+      }
+    }
+  });
+  return data;
+}
