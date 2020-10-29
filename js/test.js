dbf5e749acac6234077f3b88721d17a4ce1c37e8
Aleksahappy <aleksa.happy@gmail.com>
2020-10-29 13:01:24 +0400

    В test.js добавлено получение случайного числа

--- js/test.js
+++ js/test.js
@@ -9,3 +9,10 @@
 function toggle(event) {
   event.currentTarget.classList.toggle('checked');
 }
+
+// Случайное целое число от min до max:
+
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
