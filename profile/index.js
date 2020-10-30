e9c8981bf1944ef1b65d7bf0fa3197ab62f3250f
Aleksahappy <aleksa.happy@gmail.com>
2020-10-30 10:14:14 +0400

    Работа над страницей профиля (profile):
    - доработана верстка страницы (добавлены правильные атрибуты и ключи)
    - доработаны стили страницы
    - написан функционал по преобразованию полученных данных (convertData)
    - написан функционал по открытию окна с формой (для ее заполнения перед открытием) (openProfilePopUp)
    - доработан функционал отправки данных формы на сервер и логика действий при получении ответа сервера
    - json обновлен реальными данными

--- profile/index.js
+++ profile/index.js
@@ -1,27 +1,34 @@
 'use strict';
 
+// Глобальные переменные:
+
+var data;
+
 // Запуск страницы профиля:
 
 startProfilePage();
 
 function startProfilePage() {
-  sendRequest(`../json/profile.json`)
-  // sendRequest(urlRequest.main, {action: 'profile'})
+  // sendRequest(`../json/profile.json`)
+  sendRequest(urlRequest.main, {action: 'profile'})
   .then(result => {
-    // console.log(result);
-    var data = JSON.parse(result);
-    // console.log(data);
-    initPage(data);
-  })
-  .catch(err => {
-    console.log(err);
+    data = JSON.parse(result);
     initPage();
+  })
+  .catch(error => {
+    console.log(error);
+    loader.hide();
+    alerts.show('Во время загрузки страницы произошла ошибка. Попробуйте позже.');
   });
 }
 
 // Инициализация страницы:
 
-function initPage(data = []) {
+function initPage() {
+  if (!data || isEmptyObj(data)) {
+    return;
+  }
+  convertData();
   fillTemplate({
     area: '.profile-info .content',
     items: data
@@ -30,26 +37,49 @@ function initPage(data = []) {
   loader.hide();
 }
 
+// Преобразование полученных данных:
+
+function convertData() {
+  data.gender_text = data.gender == '1' ? 'муж.' : 'жен.';
+  data.phone = convertPhone(data.phone);
+  data.work_phone = convertPhone(data.phone);
+}
+
+// Открытие всплывающего окна с формой:
+
+function openProfilePopUp() {
+  fillForm('#profile-form', data, true);
+  openPopUp('#profile-edit');
+}
+
 // Отправка формы на сервер:
 
 function sendProfile(formData) {
-  formData.forEach((value, key) => {
-    console.log(key, value);
-  });
-  formData.append('action', 'send_profile');
+  formData.append('action', 'profile_save');
   sendRequest(urlRequest.main, formData, 'multipart/form-data')
   .then(result => {
+    result = JSON.parse(result);
     console.log(result);
-    var data = JSON.parse(result);
-    fillTemplate({
-      area: '.profile-info .content',
-      items: data
-    });
+    if (result.data && !isEmptyObj(result.data)) {
+      data = result.data;
+      convertData();
+      fillTemplate({
+        area: '.profile-info .content',
+        items: data
+      });
+      closePopUp(null, '#profile-edit');
+    } else {
+      if (result.error) {
+        alerts.show(result.error);
+      } else {
+        alerts.show('Ошибка в отправляемых данных. Перепроверьте и попробуйте еще раз.');
+      }
+    }
+    hideElement('#profile-edit .loader');
   })
   .catch(error => {
     console.log(error);
+    alerts.show('Ошибка сервера. Попробуйте позже.');
     hideElement('#profile-edit .loader');
-    closePopUp(null, '#profile-edit');
-    alerts.show('Данные не были отправлены. Попробуйте еще раз.');
   })
 }
