99ce2dc6e7ee4d213eb618ad17c72ec3991ff443
Aleksahappy <aleksa.happy@gmail.com>
2020-10-30 14:47:04 +0400

    Работа над страницей сертификатов:
    1) Верстка и стили:
    - переделана верстка страницы с использованием верстки панели управления, а также с учетом того, что адаптивные фильтры будут созданы динамически
    - переделаны стили под новую верстку
    2) Функционал:
    - написан функционал для получения данных о брендах из описаний сертификатов
    - данные о брендах добавлены в глобальную переменную для использования фильтрами и в данные о сертификатах
    - добавлена инициализация фильтров (для динамического построения адаптивных фильтров)
    - доработана загрузка данных на страницу в связи с именениями в верстке
    - доработан функционал поиска по ключевым словам
    - написан функционал поиска по бренду

--- certificates/index.js
+++ certificates/index.js
@@ -1,64 +1,71 @@
 'use strict';
 
-// fillTemplate в дальнейшем заменить на загрузку через универсальную функцию подгрузки по скроллу
-// Везде рендерить через универсальную функцию подгрузки по скроллу
+// Глобальные переменные:
 
-//=====================================================================================================
-// Первоначальные данные для работы:
-//=====================================================================================================
-
-// Константы:
-
-var items = [];
-
-// Динамически изменяемые переменные:
-
-var selectedItems = '',
-    filterItems = [];
+var items = [],
+    brands = [], // ['509', 'Abom', 'BCA', 'EVS', 'FXR', 'FullT', 'Helmetex', 'Jethwear', 'Ogio', 'PowerTools', 'SIXS', 'Shark', 'Spy Optic', 'Tobe'];
+    selectedItems = '';
 
 // Запускаем рендеринг страницы сертификатов:
 
 startCertPage();
 
-//=====================================================================================================
-// При запуске страницы:
-//=====================================================================================================
-
 // Запуск страницы сертификатов:
 
 function startCertPage() {
-  // sendRequest(`../json/certificates.json`)
-  sendRequest(urlRequest.main, {action: 'files', data: {type: 'cert'}})
+  sendRequest(`../json/certificates.json`)
+  // sendRequest(urlRequest.main, {action: 'files', data: {type: 'cert'}})
   .then(result => {
     items = JSON.parse(result);
-    convertData();
     initPage();
   })
-  .catch(err => {
-    console.log(err);
-    initPage();
+  .catch(error => {
+    console.log(error);
+    loader.hide();
+    alerts.show('Во время загрузки страницы произошла ошибка. Попробуйте позже.');
   });
 }
 
 // Инициализация страницы:
 
 function initPage() {
-  fillTemplate({
-    area: '#cert-data',
-    items: items
-  });
+  if (!items || !items.length) {
+    return;
+  }
   convertData();
+  loadData();
   initSearch('#cert-search', findCert);
-  initCalendar('#cert-range');
-  initDropDown('#cert-brand', selectBrand);
-  initCalendar('#calendar');
+  initDropDown('#cert-brand', selectBrand, brands, 'Сбросить');
+  initFilter('#cert', {
+    filters: {
+      'brands': {
+        title: 'По бренду',
+        filter: 'select',
+        items: brands,
+        isOpen: true
+      }
+    },
+    isHide: false
+  });
   loader.hide();
 }
 
-// Преобразование данных:
+// Преобразование полученных данных:
 
 function convertData() {
   items.forEach(el => {
+    var curBrands = [];
+    var findBrands = el.descr.match(/"\w{3,}"/gm);
+    if (findBrands) {
+      findBrands.forEach(brand => {
+        brand = brand.replace(/"/g, '');
+        if (brands.indexOf(brand) === -1) {
+          curBrands.push(brand);
+          brands.push(brand);
+        }
+      });
+    };
+    el.brands = curBrands;
     el.search = [];
     for (var key in el) {
       if (el[key] == 0 || el[key]) {
@@ -67,54 +74,56 @@ function convertData() {
     }
     el.search = el.search.join(',').replace(/\s/g, ' ');
   });
+  brands.sort();
+}
+
+// Загрузка данных на страницу:
+
+function loadData(data) {
+  if (data) {
+    if (data.length) {
+      hideElement('.notice');
+      fillTemplate({
+        area: '.cert-data',
+        items: selectedItems
+      });
+      showElement('.cert-data');
+    } else {
+      hideElement('.cert-data');
+      showElement('.notice');
+    }
+  } else {
+    fillTemplate({
+      area: '.cert-data',
+      items: items
+    });
+    hideElement('.notice');
+    showElement('.cert-data');
+  }
 }
 
 // Поиск по ключевым словам:
 
 function findCert(search, textToFind) {
   if (textToFind) {
+    clearDropDown('#cert-brand');
     var regEx = new RegExp(textToFind, 'gi');
     selectedItems = items.filter(el => el.search.search(regEx) >= 0);
-    if (selectedItems.length) {
-      hideElement('#notice');
-      fillTemplate({
-        area: '#cert-data',
-        items: selectedItems
-      });
-      showElement('#cert-data');
-    } else {
-      hideElement('#cert-data');
-      showElement('#notice');
-    }
+    loadData(selectedItems);
   } else {
-    fillTemplate({
-      area: '#cert-data',
-      items: items
-    });
-    hideElement('#notice');
-    showElement('#cert-data');
+    loadData();
   }
 }
 
-// Поиск по дате:
+// Фильтр по бренду:
 
-function selectDate() {
-
-}
-
-// Поиск по бренду:
-
-function selectBrand() {
-
-}
-
-// Загрузка/просмотр сертификата:
-
-function getCert(id, mode) {
-  event.preventDefault();
-  if (mode) {
-    sendRequest(urlRequest.main, {action: 'files', data: {type: 'cert', mode: 'view', id: id}});
+function selectBrand(event) {
+  var value = event.target.dataset.value;
+  if (value === 'default') {
+    loadData();
   } else {
-    sendRequest(urlRequest.main, {action: 'files', data: {type: 'cert', id: id}});
+    clearSearch('#cert-search');
+    selectedItems = items.filter(el => el.brands.indexOf(value) >= 0);
+    loadData(selectedItems);
   }
 }
