ed9ead7465db1c3860866527f4f02e7a22cbc8c5
Aleksahappy <aleksa.happy@gmail.com>
2020-11-06 20:14:00 +0400

    Работа над страницей заказов:
    - доработка верстки, стилей и функционала в соответствии с изменениями в верстке, стилях и функционале таблиц на странице заказов
    - добавлена верстка для адаптивной таблицы
    - написаны стили и медиа-стили для адаптивной таблицы
    - на страницу заказов модулем добавлено всплывающее окно отгрузок
    - подключено открытие окна отгрузок

--- orders/index.js
+++ orders/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+// Статусы заказов:
+// 1: "В пути"
+// 2: "В наличии"
+// 3: "Собран"
+// 4: "Отгружен"
+// 5: "Недопоставка"
+
 // Запускаем рендеринг страницы заказов:
 
 startOrdersPage();
@@ -7,8 +14,8 @@ startOrdersPage();
 // Запуск страницы заказов:
 
 function startOrdersPage() {
-  // sendRequest(`../json/orders.json`)
-  sendRequest(urlRequest.main, {action: 'orderslist'})
+  sendRequest(`../json/orders.json`)
+  // sendRequest(urlRequest.main, {action: 'orderslist'})
   .then(result => {
     var data = JSON.parse(result);
     // console.log(data);
@@ -35,63 +42,75 @@ function initPage(data) {
       search: 'Поиск по типу заказа, номеру, контрагенту, заказчику...',
       setting: true
     },
-    head: true,
-    trFunc: 'onclick=showOrder(event,#id#)',
-    cols: [{
-      title: 'Заказ',
-      width: '10%',
-      content: `<div class="row">
-                  <a href="" class="download icon"></a>
-                  <div>
-                    <div>#order_number#</div>
-                    <div class="text light">#order_date#</div>
-                  </div>
-                </div>`
-    }, {
-      title: 'Статус заказа',
-      key: 'order_status',
-      sort: 'text',
-      search: 'usual',
-      filter: true
-    }, {
-      title: 'Контрагент',
-      width: '15%',
-      key: 'contr_name',
-      sort: 'text',
-      search: 'usual'
-    }, {
-      title: 'Заказчик',
-      width: '15%',
-      key: 'user_fio',
-      sort: 'text',
-      search: 'usual'
-    }, {
-      title: 'Тип заказа',
-      key: 'order_type',
-      sort: 'text',
-      search: 'usual',
-      filter: true
-    }, {
-      title: 'Сумма счета',
-      align: 'right',
-      key: 'order_sum',
-      sort: 'numb',
-      search: 'usual'
-    }, {
-      title: 'ДЗ/КЗ',
-      align: 'right',
-      key: 'debt',
-      sort: 'numb',
-      search: 'usual'
-    }, {
-      title: 'Состояние товаров',
-      class: 'pills',
-      width: '20%',
-      content: '<div class="pill c10 #type# #display#">#sum#</div>'
-    }],
-    sub: [{area: '.pill', items: 'sum'}]
-  }
+    desktop: {
+      head: true,
+      trFunc: 'onclick=showOrder(event,#id#)',
+      sub: [{area: '.pill', items: 'sum'}],
+      cols: [{
+        title: 'Заказ',
+        width: '10%',
+        keys: ['order_number', 'order_date'],
+        content: `<div class="row">
+                    <div class="download icon" onclick="openPopUp('#shipment')"></div>
+                    <div>
+                      <div>#order_number#</div>
+                      <div class="text light">#order_date#</div>
+                    </div>
+                  </div>`
+      }, {
+        title: 'Статус заказа',
+        keys: ['order_status']
+      }, {
+        title: 'Контрагент',
+        width: '15%',
+        keys: ['contr_name']
+      }, {
+        title: 'Заказчик',
+        width: '15%',
+        keys: ['user_fio']
+      }, {
+        title: 'Тип заказа',
+        keys: ['order_type']
+      }, {
+        title: 'Сумма счета',
+        align: 'right',
+        keys: ['order_sum']
+      }, {
+        title: 'ДЗ/КЗ',
+        align: 'right',
+        keys: ['debt']
+      }, {
+        title: 'Состояние товаров',
+        class: 'pills',
+        width: '15%',
+        content: '<div class="pill c10 #type# #display#">#sum#</div>'
+      }]
+    },
+    sorts: {
+      'order_date': {title: 'По дате заказа', type: 'date'},
+      'order_status': {title: 'По статусу заказа', type: 'text'},
+      'contr_name': {title: 'По контрагенту', type: 'text'},
+      'user_fio': {title: 'По заказчику', type: 'text'},
+      'order_type': {title: 'По типу заказа', type: 'text'},
+      'order_sum': {title: 'По сумме счета', type: 'numb'},
+      'debt': {title: 'По ДЗ/КЗ', type: 'numb'}
+    },
+    filters: {
+      'order_number': {title: 'По номеру заказа', search: 'usual'},
+      'order_status': {title: 'По статусу заказа', search: 'usual', filter: 'checkbox'},
+      'contr_name': {title: 'По контрагенту', search: 'usual'},
+      'user_fio': {title: 'По заказчику', search: 'usual'},
+      'order_type': {title: 'По типу заказа', search: 'usual', filter: 'checkbox'},
+      'order_sum': {title: 'По сумме счета', search: 'usual'},
+      'debt': {title: 'По ДЗ/КЗ', search: 'usual'}
+    }
+  };
   initTable('#orderslist', settings);
+  fillTemplate({
+    area: '.table-adaptive',
+    items: data,
+    sub: [{area: '.pill', items: 'sum'}]
+  });
   loader.hide();
 }
 
