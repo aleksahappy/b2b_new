409aeca5832575ff2ca8a68c071864ddd640250e
Aleksahappy <aleksa.happy@gmail.com>
2020-12-16 10:53:00 +0400

    Работа над сертификатами:
    - добавлен объект для сопоставления названий брендов (brandNames)
    - в связи с изменениями в структуре данных переделано регулярное выражение для получения списка брендов из описания сертификатов (convertData)
    - добавление брендов в выпадающий список фильтра и в данные сертификата сделано с использованием сопоставления названий для единообразного написания как в каталоге (convertData)
    - в данные для поиска добавлено только описание и название сертификата (convertData)
    - в поиск по ключевым словам подключена подсветка найденного в тексте (findCert)
    - обновлен тестовый json измененными реальными данными

--- certificates/index.js
+++ certificates/index.js
@@ -6,6 +6,40 @@ var items = [],
     itemsToLoad,
     brands = [];
 
+var brandNames = {
+  '509': '509, 509 ALTITUDE',
+  'Abom': 'ABOM',
+  'BCA': 'BCA',
+  'EVS': 'EVS',
+  'FXR': 'FXR, FXR RACING, FXRMNT',
+  'FullT': 'FullT',
+  'Helmetex': 'Helmetex',
+  'Jethwear': 'JETHWEAR, JW, JWR',
+  'Ogio': 'OGIO',
+  'Shark': 'SHARK',
+  'SIXS': 'SIXS, SIX2',
+  'Spy Optic': 'SPY, SPY Optic, SPY Optics',
+  'Tobe': 'TOBE, TOBE OUTWEAR',
+  'Troy Lee Designs': 'TROY LEE DESIGNS',
+  'YETI': 'YETI',
+  'Garland': 'Garland',
+  'Hord': 'Hord',
+  'King Dolphin': 'King Dolphin',
+  'LA Sleeve': 'LA Sleeve',
+  'SAM-Tech': 'SAM-Tech',
+  'SPI': 'SPI',
+  'SeaFlo': 'SEAFLO',
+  'Skinz': 'SKINZ',
+  'Skipper': 'SKIPPER',
+  'TMC': 'TMC',
+  'Tecnoseal': 'Tecnoseal'
+}
+
+// 'FLY RACING'
+// 'HMK'
+// 'NEAL'
+// 'ONEAL'
+
 // Запуск страницы сертификатов:
 
 startCertPage();
@@ -52,10 +86,15 @@ function initPage() {
 function convertData() {
   items.forEach(el => {
     var itemBrands = [];
-    var findBrands = el.descr.match(/"\w{2,}\s{0,1}\w*"/gm);
+    var findBrands = el.descr.match(/"\w{2,}[^"]*"/gm);
     if (findBrands) {
       findBrands.forEach(brand => {
-        brand = brand.replace(/"/g, '');
+        brand = brand.replace(/"/g, '').toLowerCase();
+        for (var key in brandNames) {
+          if (brandNames[key].toLowerCase().indexOf(brand) >= 0) {
+            brand = key;
+          }
+        }
         itemBrands.push(brand);
         if (brands.indexOf(brand) === -1) {
           brands.push(brand);
@@ -63,7 +102,7 @@ function convertData() {
       });
     };
     el.brands = itemBrands;
-    el.search = convertToString(el);
+    el.search = el.descr + ';' + el.title;
   });
   brands.sort();
   itemsToLoad = items;
@@ -93,6 +132,9 @@ function findCert(search, textToFind) {
     itemsToLoad = items;
   }
   loadData();
+  if (textToFind && itemsToLoad.length) {
+    highlightText('#cert', textToFind);
+  }
 }
 
 // Фильтр по бренду:
