4a3560052a00af61db3429f9febbf81afdbd3458
Aleksahappy <aleksa.happy@gmail.com>
2020-12-21 16:19:04 +0400

    Работа над артбордом:
    - доработана верстка блока фильтров в артборде для полного соответствия фильтрам, формирующимся динамически
    - демонстрационные функции перенесены из файла test.js в index.js артборда

--- artboard/index.js
+++ artboard/index.js
@@ -171,3 +171,21 @@ fillTemplate({
 });
 
 loader.hide();
+
+
+//=====================================================================================================
+// Функции для презентации верстки с тестовыми json:
+//=====================================================================================================
+
+// Переключение состояния элементов на checked:
+
+function toggle(event) {
+  event.currentTarget.classList.toggle('checked');
+}
+
+// Случайное целое число от min до max:
+
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
